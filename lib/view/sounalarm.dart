3afad4426af829c63e6c459af5dfd4f790490c1e
t1tanontop <tgarciadecarlo@gmail.com>
2025-02-05 18:51:46 -0300

    5/2/2025
    .

--- lib/view/sounalarm.dart
+++ lib/view/sounalarm.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class SoundAlarm extends StatefulWidget {
@@ -8,7 +9,9 @@ class SoundAlarm extends StatefulWidget {
 }
 
 class _SoundAlarmState extends State<SoundAlarm> {
+  String selectedTone = 'Cascada';
   double _currentVolume = 0.5;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,67 +45,152 @@ class _SoundAlarmState extends State<SoundAlarm> {
           Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              SizedBox(height: 40),
+              const SizedBox(height: 40),
               Center(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Container(
-                        width: 310,
-                        height:
-                            100, // Ajusta la altura para acomodar el nuevo texto
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF1C1C1C),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Row(
-                            children: [
-                              const Padding(
-                                padding: EdgeInsets.all(4.0),
-                                child: Icon(
-                                  Icons.music_note,
-                                  color: Color(0xff2643d4),
-                                  size: 50,
+                      ExpansionTile(
+                        title: Row(
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(4.0),
+                              child: Icon(
+                                Icons.music_note,
+                                color: Color(0xff2643d4),
+                                size: 50,
+                              ),
+                            ),
+                            const SizedBox(
+                                width: 10), // Espacio entre el icono y el texto
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text(
+                                  'Melodias',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontFamily: 'JosefinSans-Light',
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(
-                                  width:
-                                      10), // Espacio entre el icono y el texto
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  const Text(
-                                    'Melodias',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontFamily: 'JosefinSans-light',
-                                    ),
+                                const SizedBox(
+                                    height: 5), // Espacio entre los textos
+                                Text(
+                                  selectedTone,
+                                  style: const TextStyle(
+                                    color: Color(0xff2643d4),
+                                    fontSize: 15,
+                                    fontFamily: 'JosefinSans-Regular',
                                   ),
-                                  const SizedBox(
-                                      height: 5), // Espacio entre los textos
-                                  GestureDetector(
-                                    child: const Text(
-                                      'Cascada',
-                                      style: TextStyle(
-                                        color: Color(0xff2643d4),
-                                        fontSize: 15,
-                                        fontFamily: 'JosefinSans-Regular',
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
+                        children: [
+                          ListTile(
+                            title: const Text(
+                              'Cascada',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontFamily: 'JosefinSans-Regular',
+                              ),
+                            ),
+                            trailing: IconButton(
+                              icon: Icon(
+                                Icons.check_circle,
+                                color: selectedTone == 'Cascada'
+                                    ? Colors.blue
+                                    : Colors.grey, // Cambio de color
+                              ),
+                              onPressed: () {
+                                setState(
+                                  () {
+                                    selectedTone = 'Cascada';
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                          ListTile(
+                            title: const Text(
+                              'Lluvia',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontFamily: 'JosefinSans-Regular',
+                              ),
+                            ),
+                            trailing: IconButton(
+                              icon: Icon(
+                                Icons.check_circle,
+                                color: selectedTone == 'Lluvia'
+                                    ? Colors.blue
+                                    : Colors.grey, // Cambio de color
+                              ),
+                              onPressed: () {
+                                setState(
+                                  () {
+                                    selectedTone = 'Lluvia';
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                          ListTile(
+                            title: const Text(
+                              'Pajaros',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontFamily: 'JosefinSans-Regular',
+                              ),
+                            ),
+                            trailing: IconButton(
+                              icon: Icon(
+                                Icons.check_circle,
+                                color: selectedTone == 'Pajaros'
+                                    ? Colors.blue
+                                    : Colors.grey, // Cambio de color
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  selectedTone = 'Pajaros';
+                                });
+                              },
+                            ),
+                          ),
+                          ListTile(
+                            title: const Text(
+                              'Olas del Mar',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontFamily: 'JosefinSans-Regular',
+                              ),
+                            ),
+                            trailing: IconButton(
+                              icon: Icon(
+                                Icons.check_circle,
+                                color: selectedTone == 'Olas del Mar'
+                                    ? Colors.blue
+                                    : Colors.grey, // Cambio de color
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  selectedTone = 'Olas del Mar';
+                                });
+                              },
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 20,
                       ),
                       Container(
@@ -157,7 +245,7 @@ class _SoundAlarmState extends State<SoundAlarm> {
                                       fontSize: 16,
                                       fontFamily: 'JosefinSans-Regular',
                                     ),
-                                  ), // E // Espacio entre los texto
+                                  ),
                                 ],
                               ),
                             ],
